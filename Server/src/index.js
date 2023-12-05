5b944fab0494189aa9466333f47371cf23ca425e
Ezequiel Heick <ezequielheick@gmail.com>
2023-12-05 16:21:02 -0300

    promises

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,28 +1,17 @@
 const http = require ("http")
 const PORT = 3001
 const characters = require("./utils/data")
+const getCharById = require("./controllers/getCharById")
+
 http
     .createServer((req, res)=>{
     res.setHeader('Access-Control-Allow-Origin', '*')
 
     if(req.url.includes("/rickandmorty/character")){
         const id = req.url.split("/").pop()
-        const character = characters.find(char=>char.id===Number(id))
-
-        if(character){
-           return res
-                .writeHead(200,{"content-type": "application/json"})
-                .end(JSON.stringify(character))
-        } else {
-           return res
-                .writeHead(404,{"content-type": "application/json"})
-                .end(JSON.stringify({message: "Character Not Found"}))
-        }
-
+        getCharById(res, id)   
     }
-       return res
-            .writeHead(404,{"content-type": "application/json"})
-            .end(JSON.stringify({message: "Wrong URL"}))
+
     
 })
     .listen(PORT, "127.0.0.1", 
