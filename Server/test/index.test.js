accfdd7adddaf5687f3fc24468968f46b5ce0e53
Ezequiel Heick <ezequielheick@gmail.com>
2023-12-12 23:41:48 -0300

    testing final

--- Server/test/index.test.js
+++ Server/test/index.test.js
@@ -23,12 +23,48 @@ describe("Test de RUTAS", () =>{
             await agent.get('/rickandmorty/character/123456789').expect(500)
         })
     })
-
     describe("GET /rickandmorty/login", () => {
         it("retorna objeto {access: true } con credenciales correctas", async () =>{
+            const response = await agent.get("/rickandmorty/login?email=ejemplo@gmail.com&password=123456")
+            expect(response.body.access).tobe({ access: true})
+        })
+    })
+    describe("GET /rickandmorty/login", () => {
+        it("retorna objeto {access: false } con credenciales incorrectas", async () =>{
             const response = await agent.get("/rickandmorty/login?email=ejemplo@gmail.com&password=12345678")
             expect(response.body).toEqual({ access: false})
         })
+    })
+
+    describe("POST /rickandmorty/fav", () => {
+        const character1 = { id: 1, name: "Rick"}
+        const character2 = { id: 2, name: "Morty"}
+        it("Devuelve array con personaje enviado por body", async () => {
+            const response = await agent
+                .post("/rickandmorty/fav")
+                .send(character1)
+            expect(response.body).toEqual([character1])
+        })
+        it("Devuelve array con personaje enviado por body", async () => {
+            const response = await agent
+                .post("/rickandmorty/fav")
+                .send(character1)
+            expect(response.body).toEqual([character1])
+            expect(response.body).toEqual([character2])
+        })
+    describe("DELETE /rickandmorty/fav/:id", () => {
+        it("Devuelve array con personaes si no elimina ningun personaje", async() => {
+            const response = await agent.delete("/rickandmorty/fav/15")
+            expect(response.body), toContaininEqual(character1)
+            expect(response.body), toContaininEqual(character2)           
+        })
+        it("Devuelve array sin personaes del id enviado", async() => {
+            const response = await agent.delete("/rickandmorty/fav/15")
+            expect(response.body), not.toContaininEqual(character1)
+            expect(response.body), toContaininEqual(character2)           
+        })
+
+    })
     })
 
 })
